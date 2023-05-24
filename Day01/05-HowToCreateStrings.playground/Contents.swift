047480cc0a6afd8fcdba0de9c8af745a002b0aeb
Shaun Hevey <shaun.hevey@gmail.com>
2023-05-24 20:44:33 +1000

    Completing Day01

---
+++ Day01/05-HowToCreateStrings.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+let actor = "Denzel Washington"
+let quote = "Then he tapped a sign saying \"Believe\" an walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
+print(greeting.uppercased())
+
+print(actor.hasPrefix("Den"))
