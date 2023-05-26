1e5acd6d1ff359f4e3fe386fd0ee09c0b5bf844f
Shaun Hevey <shaun.hevey@gmail.com>
2023-05-27 06:49:21 +1000

    Adding Day04

---
+++ Day04/02-Checkpoint2.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+var tripLocations = ["Melbourne", "Sydney", "Brisbane", "Melbourne"]
+var visitedLocations = Set(tripLocations)
+
+print("I have made \(tripLocations.count) in the last year")
+print("I have visited \(visitedLocations.count) different cities")
