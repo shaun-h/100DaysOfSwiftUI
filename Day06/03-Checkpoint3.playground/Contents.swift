13689d636596e91ccea565dfe51afbb2ff1c581f
Shaun Hevey <shaun.hevey@gmail.com>
2023-05-29 06:34:02 +1000

    Adding Day06

---
+++ Day06/03-Checkpoint3.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+for i in 1...100 {
+    var output = ""
+    if i.isMultiple(of: 3) {
+        output += "Fizz"
+    }
+    if i.isMultiple(of: 5) {
+        output += "Buzz"
+    }
+    if output.isEmpty {
+        output = "\(i)"
+    }
+    
+    print(output)
+}
