7352715918de674fabf7e2edbc3a3426775efcee
Shaun Hevey <shaun.hevey@gmail.com>
2023-06-02 07:33:56 +1000

    Adding Day10

---
+++ Day10/03-HowToTakeActionWhenAPropertyChanges.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
---
+++ Day10/04-HowToCreateCustomInitializers.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+struct Player {
+    let name: String
+    let number: Int
+
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+
+print(player.number)
+
