12995f2426a02dc6ebecf0ab6cec5679f7130181
Shaun Hevey <shaun.hevey@gmail.com>
2023-06-09 22:32:18 +1000

    Adding Day20

---
+++ Day20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Shaun Hevey on 9/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
