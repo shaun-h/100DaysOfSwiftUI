2db7bf6cddaa1715caa3fc9f839176d3eebe6ee7
Shaun Hevey <shaun.hevey@gmail.com>
2023-06-16 22:45:54 +1000

    Adding Day27

---
+++ Day27/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Shaun Hevey on 16/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
