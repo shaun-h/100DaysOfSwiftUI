3583411c7d46b081455f082091707bc022f2c8f8
Shaun Hevey <shaun.hevey@gmail.com>
2023-06-16 23:07:44 +1000

    Adding Day28

---
+++ Day28/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Shaun Hevey on 16/6/2023.
+//
+
+import CoreML
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertMessage = ""
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Text(alertMessage)
+                        .font(.headline)
+                }
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                    .onChange(of: wakeUp) { _ in
+                        calculateBedtime()
+                    }
+                Section {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                        .onChange(of: sleepAmount) { _ in
+                            calculateBedtime()
+                        }
+                        
+                }
+                Section {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Picker(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) { number in
+                            Text("\(number)")
+                        }
+                    }
+                    .onChange(of: coffeeAmount) { _ in
+                        calculateBedtime()
+                    }
+                }
+            
+            }
+            .navigationTitle("BetterRest")
+        }.onAppear {
+            calculateBedtime()
+        }
+    }
+    
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertMessage = "Your ideal bedtime is " + sleepTime.formatted(date: .omitted, time: .shortened)
+        }
+        catch {
+            alertMessage = "Error: Sorry, there was a problem calculating your bedtime."
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
