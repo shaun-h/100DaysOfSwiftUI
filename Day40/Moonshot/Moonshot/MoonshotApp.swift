a275278185af07af84e5314d67966efe69a258c0
Shaun Hevey <shaun.hevey@gmail.com>
2023-06-27 07:00:25 +1000

    Adding Day40

---
+++ Day40/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Shaun Hevey on 26/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
