7d9bbe88402d2cbe11b35599b1e19c872ece7b52
Shaun Hevey <shaun.hevey@gmail.com>
2023-07-02 22:05:48 +1000

    Adding Day52

---
+++ Day52/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Shaun Hevey on 2/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
