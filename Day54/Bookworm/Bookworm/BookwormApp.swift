5c6084a7693230b171066d5715021bf5f53dc418
Shaun Hevey <shaun.hevey@gmail.com>
2023-07-03 13:23:16 +1000

    Adding Day54

---
+++ Day54/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,21 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Shaun Hevey on 3/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    @StateObject private var dataController = DataController()
+
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
---
+++ Day54/Bookworm/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Shaun Hevey on 3/7/2023.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("🙈")
+        case 2:
+            Text("😕")
+        case 3:
+            Text("😬")
+        case 4:
+            Text("🙂")
+        default:
+            Text("🤩")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
