d99c1360f0161509135b38a8d81472b4279876c0
Shaun Hevey <shaun.hevey@gmail.com>
2023-07-11 06:08:47 +1000

    Adding Day65

---
+++ Day65/Instafilter/Instafilter/ImageSaver.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Shaun Hevey on 10/7/2023.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
