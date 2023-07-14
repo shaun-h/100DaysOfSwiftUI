fbdbc09b3fa60cdf3f69ecd8eb23b504b047de25
Shaun Hevey <shaun.hevey@gmail.com>
2023-07-14 22:17:12 +1000

    Adding Day76

---
+++ Day76/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Shaun Hevey on 26/6/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
