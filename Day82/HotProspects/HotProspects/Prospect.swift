a5869ab5ee185fba838b50913f758f032c83db12
Shaun Hevey <shaun.hevey@gmail.com>
2023-07-16 22:24:22 +1000

    Adding Day82

---
+++ Day82/HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Shaun Hevey on 16/7/2023.
+//
+
+import Foundation
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+
+    init() {
+        self.people = []
+    }
+}
---
+++ Day82/HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,75 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Shaun Hevey on 16/7/2023.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    @EnvironmentObject var prospects: Prospects
+    
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
+    
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button {
+                    let prospect = Prospect()
+                    prospect.name = "Paul Hudson"
+                    prospect.emailAddress = "paul@hackingwithswift.com"
+                    prospects.people.append(prospect)
+                } label: {
+                    Label("Scan", systemImage: "qrcode.viewfinder")
+                }
+            }
+        }
+    }
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var prospects: Prospects = Prospects()
+    static var previews: some View {
+        ProspectsView(filter: .none)
+            .environmentObject(prospects)
+    }
+}
