cea60380101e74108e46599c60b30601f20e9c2b
Shaun Hevey <shaun.hevey@gmail.com>
2023-07-16 22:39:48 +1000

    Adding Day83

---
+++ Day83/HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,28 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Shaun Hevey on 16/7/2023.
+//
+
+import Foundation
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+
+    init() {
+        self.people = []
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+    }
+}
