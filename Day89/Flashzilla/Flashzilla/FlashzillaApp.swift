cf90d8449c5ed5f8e32619b1edd13be8e828e7fa
Shaun Hevey <shaun.hevey@gmail.com>
2023-07-17 21:50:03 +1000

    Adding Day89

---
+++ Day89/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Shaun Hevey on 17/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
