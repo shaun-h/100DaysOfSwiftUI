4a51cbe4e4838ee81416c796feadb3d8fb1ce27a
Shaun Hevey <shaun.hevey@gmail.com>
2023-07-18 07:46:43 +1000

    Adding Day92

---
+++ Day92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Shaun Hevey on 17/7/2023.
+//
+
+import SwiftUI
+
+extension VerticalAlignment {
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in d: ViewDimensions) -> CGFloat {
+            d[.top]
+        }
+    }
+
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+struct ContentView: View {
+    var body: some View {
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@twostraws")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+
+            VStack {
+                Text("Full name:")
+                Text("PAUL HUDSON")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
