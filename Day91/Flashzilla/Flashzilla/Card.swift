752deeb296be1ee746736b2717a69dbbb3bcce15
Shaun Hevey <shaun.hevey@gmail.com>
2023-07-17 22:59:22 +1000

    Adding Day91

---
+++ Day91/Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Shaun Hevey on 17/7/2023.
+//
+
+import Foundation
+
+struct Card: Codable, Identifiable, Hashable {
+    var id = UUID()
+    let prompt: String
+    let answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
+
